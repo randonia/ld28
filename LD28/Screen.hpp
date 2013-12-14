1e0b37e88072b29bdb5118806d55893f0fe95436
Ryan Andonian <ryanandonian@gmail.com>
2013-12-13 19:48:09 -0800

    Commented Screen class

--- LD28/Screen.hpp
+++ LD28/Screen.hpp
@@ -9,10 +9,12 @@ class Screen
 public:
 	Screen(void);
 	virtual ~Screen(void);
-
+	// The 'Tick' phase
 	virtual void update(sf::Time& delta);
+	// The 'draw' phase. Draws everything to the window
 	virtual void draw(sf::RenderWindow& window);
 protected:
+	// An identification string for debugging purposes
 	std::string ID;
 };
 
